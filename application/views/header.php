05ba63157088da551b9705b3de372696aa9fdb94
Kevin Fairbanks <kevin@kevinfairbanks.com>
2017-12-06 17:07:02 -0500

    Added Bootstrap

---
+++ application/views/header.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('');
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title><?= $pageTitle ?></title>
+		
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="<?=base_url()?>assets/images/icon.ico">
+        <!-- favicon ends -->
+
+        <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap-theme.min.css" media="screen">
+        <link rel="stylesheet" href="<?=base_url()?>assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?=base_url()?>assets/css/font-awesome-animation.min.css">
+        <link rel="stylesheet" href="<?=base_url()?>public/css/select2.min.css">
+
+        <script src="<?=base_url()?>assets/js/jquery.min.js"></script>
+        <script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+        <script src="<?=base_url()?>assets/js/select2.min.js"></script>
+        
+        
+        <!-- custom CSS -->
+        <link rel="stylesheet" href="<?= base_url() ?>assets/css/main.css">
+
+        <!-- custom JS -->
+        <script src="<?= base_url() ?>assets/js/main.js"></script>
